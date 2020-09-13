1e30e6b1f72e6d50f67b56b9e9a3b145cc40f398
leonafranco <leonardomf96@gmail.com>
2020-09-13 14:51:20 +0100

    na raça acabei

--- program.c
+++ program.c
@@ -25,11 +25,16 @@ Aluno alunos [MAX_ALUNOS];
 void escolha_menu_principal(int escolha);
 void inserir_trabalhos_praticos();
 void criar_novo_aluno();
+void remover();
+void alterar_info();
 void menu_principal();
 void ajuda();
 void sair();
 void error();
 void vernotas();
+void media_aluno();
+void media_trabalho();
+void media_turma();
 void guarda_dados();
 void carrega_dados();
 
@@ -49,12 +54,17 @@ void menu_principal() {
         printf("| 4 - Remover aluno                                 |\n");
         printf("| 5 - Apresentar as notas de todos os alunos        |\n");
         printf("| 6 - ver media                                     |\n");
-        printf("| 7 - Ajuda                                         |\n");
-        printf("| 8 - Sair                                          |\n");
+        printf("| 7 - Apresentar Média número do aluno              |\n");
+        printf("| 8 - Apresentar Média nome do aluno                |\n");
+        printf("| 9 - Aprensentar pesos e médias trabalhos práticos |\n");
+        printf("| 10- Apresentar Média da turma                     |\n");
+        printf("| 11- Ajuda                                         |\n");
+        printf("| 12- Sair                                          |\n");
+        printf("| 13- Guardar Dados                                 |\n");
         printf("-----------------------------------------------------\n");
         int opcao;
         scanf(" %d", &opcao);
-        if (opcao == 8)
+        if (opcao == 12)
             break;
         escolha_menu_principal(opcao);
     }
@@ -74,6 +84,8 @@ void criar_novo_aluno() {
 
         printf("Qual é o nome do Aluno?\n");
         fgets(nome, sizeof(nome), stdin);
+        strtok(nome, "\n");
+
         
         printf("Qual o número do Aluno?\n");
         scanf("%d", &num_aluno);
@@ -114,7 +126,7 @@ void inserir_trabalhos_praticos() {
         count = 1;
 
         getchar();
-        printf("Deseja Inserir ou alterar notas?");
+        printf("Deseja Inserir ou alterar notas?\n");
         scanf("%d", &inserir_alterar);
         
 
@@ -175,7 +187,7 @@ void inserir_trabalhos_praticos() {
         }
 
 
-        printf("\n\nDeseja adicionar mais algum aluno?\n\n Sim - 1 \n Não - 2 \n\n");
+        printf("\n\nDeseja adicionar mais alguma nota?\n\n Sim - 1 \n Não - 2 \n\n");
         scanf("%d", &op);
 
         if(op == 2)
@@ -192,15 +204,15 @@ void alterar_info() {
     char nome[50];
     int op;
 
-    printf("Deseja altera nome ou peso da nota? \n1- nome 2- peso");
+    printf("\n\nDeseja altera nome ou peso da nota? \n1- nome 2- peso\n");
     scanf("%d", &op);
 
-    printf("Qual o número do aluno?");
+    printf("Qual o número do aluno?\n");
     scanf("%d", &num_aluno);
 
     if(op == 1) {
         getchar();
-        printf("Qual o nome para qual deve alterar?");
+        printf("Qual o nome para qual deve alterar?\n");
         fgets(nome, sizeof(nome), stdin);
         
         for (int i = 0; i < MAX_ALUNOS; i++)  {
@@ -209,9 +221,9 @@ void alterar_info() {
             }
         }
     }else if (op == 2) {
-        printf("Qual o numero da prova que deseja alterar?");
+        printf("Qual o numero da prova que deseja alterar?\n");
         scanf("%f", &num);
-        printf("Qual o peso que irá ter?");
+        printf("Qual o peso que irá ter?\n");
         scanf("%f", &peso);
 
         for(int i = 0; i < MAX_ALUNOS; i++ ) {
@@ -233,11 +245,12 @@ void remover() {
     int num_aluno;
     int num_trabalho;
     int op;
+    int count = 1;
 
-    printf("Deseja alterar alunos ou notas de alunos?  \n1- Aluno 2- Notas");
+    printf("Deseja alterar alunos ou notas de alunos?  \n1- Aluno 2- Notas\n");
     scanf("%d", &op);
 
-    printf("Qual o num do aluno que deseja remover?");
+    printf("Qual o num do aluno que deseja remover?\n");
     scanf("%d", &num_aluno);
 
     if(op == 1) {
@@ -247,13 +260,24 @@ void remover() {
             }
         } 
     }else if (op == 2){  
-        printf("Qual o número do trabalho que seja remover?");
+        printf("Qual o número do trabalho que seja remover?\n");
         scanf("%d", &num_trabalho);     
         for(int i = 0; i < MAX_ALUNOS; i++) {
             if(alunos[i].num_aluno == num_aluno) {
                 for(int f = 0; f < MAX_TRABALHOS; f++) {
                     if(alunos[i].trab_pratico[f].contador == num_trabalho) {
                         alunos[i].trab_pratico[f].ativo = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < MAX_ALUNOS; i++) {
+            if(alunos[i].num_aluno == num_aluno) {
+                for(int f = 0; f < MAX_TRABALHOS; f++ ) {
+                    if(alunos[i].trab_pratico[f].ativo == 1) {
+                        alunos[i].trab_pratico[f].contador = count;
+                        count++;
                     }
                 }
             }
@@ -292,7 +316,7 @@ void media_aluno() {
 
     for(int i = 0; i< MAX_ALUNOS; i++) {
         if(alunos[i].ativo == 1) {
-            printf("Aluno: %s Número de aluno:%d \n", alunos[i].nome, alunos[i].num_aluno);
+            printf("Aluno: %s | Número de aluno:%d \n", alunos[i].nome, alunos[i].num_aluno);
             for(int f = 0; f < MAX_TRABALHOS; f++){
                 if(alunos[i].trab_pratico[f].ativo == 1){
                     media_alunos = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
@@ -300,18 +324,21 @@ void media_aluno() {
                     count_trabalhos++;
                 }
             }
+            media_total = media_total/count_trabalhos;
+            printf("Média do aluno: %.2f \n\n", media_total);
+            media_alunos = 0;
+            media_total = 0;
         }
     }
-    media_total = media_total/count_trabalhos;
-    printf("Média de todos os alunos: %.2f \n\n ", media_total);
 }
 
 //! Alinea h) aprensetar media e notas do aluno com base do seu numero 7-
 void media_num_aluno() {
 
     int num_aluno;
-    float media_aluno;
-    float contador = 0;
+    float media_alunos;
+    float media_total;
+    float count_trabalhos = 0;
 
     printf("Qual o número do aluno?\n");
     scanf("%d", &num_aluno);
@@ -319,18 +346,19 @@ void media_num_aluno() {
 
     for(int i = 0; i < MAX_ALUNOS; i++) {
         if(alunos[i].num_aluno == num_aluno) {
-            printf("Nome: %s \n Número: %d \n Notas:\n", alunos[i].nome, num_aluno);
+            printf("\n\nNome: %s \nNúmero: %d \nNotas:\n", alunos[i].nome, num_aluno);
             for(int f = 0; f < MAX_TRABALHOS; f++){
                 if(alunos[i].trab_pratico[f].ativo == 1){
                     printf("Peso: %.0f Nota: %0.2f Número: %d \n",alunos[i].trab_pratico[f].peso, alunos[i].trab_pratico[f].notas, alunos[i].trab_pratico[f].contador);
-                    media_aluno += alunos[i].trab_pratico[f].notas * (alunos[i].trab_pratico[f].peso/100);
-                    contador++;
+                    media_alunos = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
+                    media_total += media_alunos;
+                    count_trabalhos++;;
                 }
             }
         }
     }
-    media_aluno = media_aluno/contador;
-    printf("A Média do aluno é %.2f", media_aluno);
+    media_total = media_alunos/count_trabalhos;
+    printf("A Média do aluno é %.2f\n\n", media_alunos);
 
 }
 
@@ -339,28 +367,34 @@ void media_num_aluno() {
 void media_nome_aluno() {
 
     char nome[50];
-    float media_aluno;
-    int contador;
+    float media_alunos;
+    float media_total;
+    float count_trabalhos = 0;
+    char *temp;
 
     getchar();
 
     printf("Qual o nome do aluno?\n");
     fgets(nome, sizeof(nome), stdin);
+    strtok(nome, "\n");
 
-    for(int i = 0; i < MAX_ALUNOS; i++) {
-        if(strcasecmp(nome, alunos[i].nome) >= 0) { 
-            printf("Nome: %s \n Número: %d \n Notas:\n", nome, alunos[i].num_aluno);
+    for(int i = 0; i < MAX_ALUNOS; i++) { 
+        if(strcmp(nome, alunos[i].nome) == 0 && alunos[i].ativo == 1) { 
+            printf("\nNome: %s \nNúmero: %d \nNotas:\n", alunos[i].nome, alunos[i].num_aluno);
             for(int f = 0; f < MAX_TRABALHOS; f++){
                 if(alunos[i].trab_pratico[f].ativo == 1){
                     printf("%.2f \n", alunos[i].trab_pratico[f].notas);
-                    media_aluno += alunos[i].trab_pratico[f].notas * (alunos[i].trab_pratico[f].peso/100);
-                    contador++;
+                    media_alunos = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
+                    media_total += media_alunos;
+                    count_trabalhos++;
                 }
             }
+            media_total = media_total/count_trabalhos;
+            printf("Média do aluno: %.2f \n\n ", media_total);
+            media_alunos = 0;
+            media_total = 0;
         }
     }
-
-    printf("A Média do aluno é %.2f", media_aluno);
 
 }
 
@@ -372,6 +406,8 @@ void media_trabalho() {
     float count_alunos = 0;
     float notas[MAX_TRABALHOS];
     int num_trabalho = 1;
+    float media_teste;
+    int ativo[MAX_TRABALHOS];
 
 
 
@@ -380,15 +416,20 @@ void media_trabalho() {
             count_alunos++;
             for(int f = 0; f < MAX_TRABALHOS; f++){
                 if(alunos[i].trab_pratico[f].ativo == 1){
-                    notas[f] = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
+                    media_teste = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
+                    notas[f] += media_teste;
+                    ativo[f] = 1;
                 }
             }
         }
     }
     for (int i = 0; i < MAX_TRABALHOS; i++) {
         notas[i] = notas[i]/count_alunos;
-        num_trabalho += i;
-        printf("Média trabalho nº %d: %.2f \n ",num_trabalho, notas[i]);
+        num_trabalho = i;
+        num_trabalho++;
+        if(ativo[i] == 1) {
+            printf("Média trabalho nº %d: %.2f\n",num_trabalho, notas[i]);
+        }
     }
     
 }
@@ -396,28 +437,32 @@ void media_trabalho() {
 //! alinea K) apresentar a media da turma
 void media_turma() {
 
-    int num_total_alunos;
     float media_alunos;
-    int contador;
     float media_total;
+    float count_trabalhos;
+    float count_alunos = 0;
+    float media_turma = 0;
 
 
-    for(int i = 0; i< MAX_ALUNOS; i++) {
+    for(int i = 0; i < MAX_ALUNOS; i++) {
         if(alunos[i].ativo == 1) {
-            printf("Aluno: %s %d \n", alunos[i].nome, alunos[i].num_aluno);
-            for(int f = 0; f < 10; f++){
+            count_alunos++;
+            count_trabalhos = 0;
+            media_alunos = 0;
+            media_total = 0;
+            for(int f = 0; f < MAX_TRABALHOS; f++){
                 if(alunos[i].trab_pratico[f].ativo == 1){
-                    media_alunos += alunos[i].trab_pratico[f].notas * (alunos[i].trab_pratico[f].peso/100);
-                    contador++;
+                    media_alunos = (alunos[i].trab_pratico[f].peso/100 * alunos[i].trab_pratico[f].notas);
+                    media_total += media_alunos;
+                    count_trabalhos++;;
                 }
             }
-            media_alunos = media_alunos/contador;
-            media_total += media_alunos;
-            num_total_alunos++;
+            media_total = media_total/count_trabalhos;
+            media_turma += media_total;
         }
     }
-
-    printf("Média de todos os alunos: %.2f ", media_alunos);
+    media_turma = media_turma/count_alunos;
+    printf("A Média da turma é %.2f\n\n", media_turma);
 }
 
 void escolha_menu_principal(int escolha) {
@@ -448,11 +493,27 @@ void escolha_menu_principal(int escolha) {
             break;
 
         case 7:
-            remover();
+            media_num_aluno();
             break;
 
         case 8:
+            media_nome_aluno();
+            break;
+
+        case 9:
+            media_trabalho();
+            break;
+
+        case 10:
+            media_turma();
+            break;
+
+        case 11:
             ajuda();
+            break;
+
+        case 13:
+            guarda_dados();
             break;
 
         default:
@@ -487,10 +548,11 @@ void guarda_dados() {
     else {
         for(int i = 0; i < MAX_ALUNOS; i ++) {
             if (alunos[i].ativo == 1) {
-                fprintf(g, "%s %d %d ", alunos[i].nome, alunos[i].ativo, alunos[i].num_aluno);
+                fprintf(g, "%s, %d, %d, ", alunos[i].nome, alunos[i].ativo, alunos[i].num_aluno);
                 for(int f = 0; f < MAX_TRABALHOS; f++) {
-                    fprintf(g, "%f %f %d %d ", alunos[i].trab_pratico[f].peso, alunos[i].trab_pratico[f].notas, alunos[i].trab_pratico[f].ativo, alunos[i].trab_pratico[f].contador);
+                    fprintf(g, "%f, %f, %d, %d,", alunos[i].trab_pratico[f].peso, alunos[i].trab_pratico[f].notas, alunos[i].trab_pratico[f].ativo, alunos[i].trab_pratico[f].contador);
                 }
+                //fprintf(g,"\n");
             }
         }
         fclose(g);
@@ -518,7 +580,7 @@ void carrega_dados() {
         while (1)
         {   //!isto é o pedaço de codigo mais nojento que alguma vez vou escrever na minha vida
 
-            fscanf(g, "%s %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d %f %f %d %d", nome, &ativo, &num_aluno, &peso[0], &nota[0], &ativo_trab[0], &contador[0] , &peso[1], &nota[1], &ativo_trab[1], &contador[1] , &peso[2], &nota[2], &ativo_trab[2], &contador[2], &peso[3], &nota[3], &ativo_trab[3], &contador[3] , &peso[4], &nota[4], &ativo_trab[4], &contador[4] , &peso[5], &nota[5], &ativo_trab[5], &contador[5], &peso[6], &nota[6], &ativo_trab[6], &contador[6] , &peso[7], &nota[7], &ativo_trab[7], &contador[7] , &peso[8], &nota[8], &ativo_trab[8], &contador[8], &peso[9], &nota[9], &ativo_trab[9], &contador[9] , &peso[10], &nota[10], &ativo_trab[10], &contador[10] , &peso[11], &nota[11], &ativo_trab[11], &contador[11], &peso[12], &nota[12], &ativo_trab[12], &contador[12] , &peso[13], &nota[13], &ativo_trab[13], &contador[13] , &peso[14], &nota[14], &ativo_trab[14], &contador[14], &peso[15], &nota[15], &ativo_trab[15], &contador[15] , &peso[16], &nota[16], &ativo_trab[16], &contador[16] , &peso[16], &nota[17], &ativo_trab[17], &contador[17], &peso[18], &nota[18], &ativo_trab[18], &contador[18] , &peso[19], &nota[19], &ativo_trab[19], &contador[19]);
+            fscanf(g, "%50[^,], %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d, %f, %f, %d, %d,", nome, &ativo, &num_aluno, &peso[0], &nota[0], &ativo_trab[0], &contador[0] , &peso[1], &nota[1], &ativo_trab[1], &contador[1] , &peso[2], &nota[2], &ativo_trab[2], &contador[2], &peso[3], &nota[3], &ativo_trab[3], &contador[3] , &peso[4], &nota[4], &ativo_trab[4], &contador[4] , &peso[5], &nota[5], &ativo_trab[5], &contador[5], &peso[6], &nota[6], &ativo_trab[6], &contador[6] , &peso[7], &nota[7], &ativo_trab[7], &contador[7] , &peso[8], &nota[8], &ativo_trab[8], &contador[8], &peso[9], &nota[9], &ativo_trab[9], &contador[9] , &peso[10], &nota[10], &ativo_trab[10], &contador[10] , &peso[11], &nota[11], &ativo_trab[11], &contador[11], &peso[12], &nota[12], &ativo_trab[12], &contador[12] , &peso[13], &nota[13], &ativo_trab[13], &contador[13] , &peso[14], &nota[14], &ativo_trab[14], &contador[14], &peso[15], &nota[15], &ativo_trab[15], &contador[15] , &peso[16], &nota[16], &ativo_trab[16], &contador[16] , &peso[16], &nota[17], &ativo_trab[17], &contador[17], &peso[18], &nota[18], &ativo_trab[18], &contador[18] , &peso[19], &nota[19], &ativo_trab[19], &contador[19]);
             if (feof(g))
                 break;
 
